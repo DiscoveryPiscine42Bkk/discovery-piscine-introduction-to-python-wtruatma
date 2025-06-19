e350f2aaefc64db8d7b7e465455a196683017a8d
wtruatma <truatmankha2547@gmail.com>
2025-06-19 08:35:51 +0000

    ex08

---
+++ cell05/ex08/aff_rev_params.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import sys
+def main():
+    params = sys.argv[1:]
+    if len(params) < 2:
+        print("none")
+    else:
+        for param in reversed(params):
+            print(param)
+main()
