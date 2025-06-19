823100baccdde2909c18bbd2916910d4cf22508b
wtruatma <truatmankha2547@gmail.com>
2025-06-19 14:43:40 +0000

    ex10

---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,12 @@
+import sys
+
+if len(sys.argv)!= 2:
+    print("none")
+else:
+    expected = sys.argv[1]
+    user_input = input("What was the parameter? ")
+    if user_input == expected:
+        print("good job!")
+    else:
+        print("Nope,sorry...")
+        
